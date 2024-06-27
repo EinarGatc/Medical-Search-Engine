6adcb0860de43071b8b5813e76d2b2eee8764599
Einar Gatchalian <egatchal@Einars-Laptop.attlocal.net>
2024-06-26 21:42:09 -0700

    Content Block Finished and Documented

--- ContentBlock.py
+++ ContentBlock.py
@@ -4,19 +4,64 @@ import requests
 import matplotlib.pyplot as plt
 import numpy as np
 import math
+"""
+    
+    Parameters
+    ----------
+    
+    Returns
+    -------
+    
+    """
+def FindContentBlock(htmlContent:bytes, consecutiveChunks:int=3) -> list[str]:
+    """Find intervals of content within the html.
+    
+    Parameters
+    ----------
+    htmlContent : bytes
+        a byte representation of the html content
+    consecutiveChunks : int
+        an integer representing the number of consecutive chunks to start with
+    
+    Returns
+    -------
+    list[str]
+        a list of tokens that contains text only within important intervals
 
-def FindContentBlock(htmlContent) -> str:
+    """
+
+    prevIntervals = None
     dsc = CreateDSC(htmlContent)
     slope, intercept = CalculateSlopeDSC(dsc)
     windowSize = ActivationFunction(len(dsc))
     # VisualizeDSC(dsc, slope, intercept)
-    slopesLow, totalLowTokens = DetermineLowSlopeSections(dsc, windowSize, slope)
-    if totalLowTokens > len(dsc) * .1 and sum([value[1] for value in slopesLow]) / len(slopesLow) <= .5 * slope:
-        return JoinIntervals(slopesLow)
     
-    return None
+    slopesLow, totalLowTokens = DetermineLowSlopeSections(dsc, windowSize, slope, consecutiveChunks)
+    while totalLowTokens > len(dsc) * .1 and sum([value[1] for value in slopesLow]) / len(slopesLow) <= .5 * slope:
+        prevIntervals = slopesLow
+        consecutiveChunks += 1
+        slopesLow, totalLowTokens = DetermineLowSlopeSections(dsc, windowSize, slope, consecutiveChunks)
+    
+    # print(sum([value[1] for value in slopesLow]) / len(slopesLow), .5 * slope)
+    
+    joinedIntervals = JoinIntervals(prevIntervals)
+    return GetContentOfIntervals(htmlContent, joinedIntervals)
 
-def JoinIntervals(slopesLow):
+def JoinIntervals(slopesLow:list[list[tuple[int,int],int]]) -> list[tuple[int,int]]:
+    """Joins intervals together
+    
+    Parameters
+    ----------
+    slopesLow : list[list[tuple[int,int],int]]
+        list of intervals and their corresponding line of best fit slopes
+
+    Returns
+    -------
+        list[tuple[int,int]]
+            list of joined intervals
+    
+    """
+
     prev = None
     result = []
     for value in slopesLow:
@@ -28,53 +73,86 @@ def JoinIntervals(slopesLow):
         else:
             result.append(prev)
             prev = interval
-    result.append(prev)
+
+    if prev:
+        result.append(prev)
+
     return result
 
 
 
-def DetermineLowSlopeSections(dsc, windowSize, slopeDSC):
+def DetermineLowSlopeSections(dsc:list[tuple[int,int]], windowSize:int, slopeDSC:int, consecutiveChunks:int) -> list[list[tuple[int,int], int]]:
+    """Determine low slope section of the dsc graph
+    
+    Parameters
+    ----------
+    dsc : list[tuple[int,int]]
+        document slope curve graph
+    windowSize : int
+        size of each chunk
+    slopeDSC : int
+        slope of line of best fit
+    consecutiveChunks : int
+        number of needed consecutive chunks
+
+    Returns
+    -------
+    list[list[tuple[int,int], int]]
+        list of important intervals and their corresponding slopes
+
+    """
+
     result = list()
     totalTokens = len(dsc)
     totalChunks = 2 * math.ceil(totalTokens/windowSize)
     lowFlag = False
-    prevSlope1 = None
-    prevSlope2 = None
+    chunks = []
+    slopes = []
+    prevSlopes = [None] * (consecutiveChunks-1)
     totalLowTokens = 0
 
-    for i in range(totalChunks-3):
-        chunk1 = (i*windowSize//2, (i+1)*windowSize//2)
-        chunk2 = ((i+1)*windowSize//2, (i+2)*windowSize//2)
-        chunk3 = ((i+2)*windowSize//2, totalTokens) if (i+3)*windowSize//2 > totalTokens else ((i+2)*windowSize//2, (i+3)*windowSize//2)
+    for i in range(totalChunks-consecutiveChunks):
+        chunks = [((i+j)*windowSize//2, (i+j+1)*windowSize//2) if (i+j+1)*windowSize//2 <= totalTokens  else ((i+j)*windowSize//2, totalTokens) for j in range(consecutiveChunks)]
+        slopes = [prevSlopes[j] if j != consecutiveChunks-1 and prevSlopes[j] else CalculateSlopeDSC(dsc[chunk[0]:chunk[1]])[0] for j, chunk in enumerate(chunks)]
+        
+        prevSlopes.clear()
+        prevSlopes = [slopes[j+1] for j in range(consecutiveChunks-1)]
 
-        slope1 = prevSlope1 if prevSlope1 else CalculateSlopeDSC(dsc[chunk1[0]:chunk1[1]])[0]
-        slope2 = prevSlope2 if prevSlope2 else CalculateSlopeDSC(dsc[chunk2[0]:chunk2[1]])[0]
-        slope3 = CalculateSlopeDSC(dsc[chunk3[0]:chunk3[1]])[0]
-
-        prevSlope1 = slope2
-        prevSlope2 = slope3
-        if slope1 > slopeDSC * .5 and slope2 > slopeDSC * .5 and slope3 > slopeDSC * .5:
+        if all(slope > slopeDSC * .5 for slope in slopes):
             lowFlag = False
-        elif slope1 <= slopeDSC * .5 and slope2 <= slopeDSC * .5 and slope3 <= slopeDSC * .5:
-            result.append([chunk1, slope1])
-            totalLowTokens += chunk1[1] - chunk1[0] + 1
+        elif all(slope <= slopeDSC * .5 for slope in slopes):
+            result.append([chunks[0], slopes[0]])
+            totalLowTokens += chunks[0][1] - chunks[1][0] + 1
             lowFlag = True
         elif lowFlag:
-            result.append([chunk1, slope1])
-            totalLowTokens += chunk1[1] - chunk1[0] + 1
+            result.append([chunks[0], slopes[0]])
+            totalLowTokens += chunks[0][1] - chunks[0][0] + 1
         
     if lowFlag:
-        result.append([chunk2, prevSlope1])
-        result.append([chunk3, prevSlope2])
-        totalLowTokens += chunk2[1] - chunk2[0] + 1
-        totalLowTokens += chunk3[1] - chunk3[0] + 1
+        [result.append([chunks[i+1], prevSlopes[i]]) for i in range(len(prevSlopes))]
+        totalLowTokens += sum([chunks[i][1] - chunks[i][0] + 1 for i in range(1, len(chunks))])
     
     return result, totalLowTokens
 
         
 
 
-def CalculateSlopeDSC(dsc):
+def CalculateSlopeDSC(dsc:list[int,int]) -> int | int:
+    """Calculate the average slope of the dsc graph
+    
+    Parameters
+    ----------
+    dsc : list[int,int]
+        document slope curve graph
+
+    Returns
+    -------
+    int
+        slope of the line of best fit
+    int
+        y-intercept of the line of best fit
+    """
+
     x_values, y_values = zip(*dsc)
 
 
@@ -88,7 +166,23 @@ def CalculateSlopeDSC(dsc):
 
     return slope, intercept
 
-def VisualizeDSC(dsc, slope, intercept) -> None:
+def VisualizeDSC(dsc:list[tuple[int,int]], slope:int, intercept:int) -> None:
+    """ Visualize the dsc graph and the line of best fit
+    
+    Parameters
+    ----------
+    dsc : list[tuple[int,int]]
+        document slope curve graph
+    slope : int
+        slope of the line of best fit
+    intercept : int
+        y-intercept of the line of best fit
+
+    Returns
+    -------
+    None
+    """
+
     x_values, y_values = zip(*dsc)
     
     x_values = np.array(x_values)
@@ -110,7 +204,29 @@ def VisualizeDSC(dsc, slope, intercept) -> None:
     # Display the plot
     plt.show()
 
-def ActivationFunction(tokenCount, minWindowSize=8, maxWindowSize=50, lower=200, upper=5000) -> int:
+def ActivationFunction(tokenCount:int, minWindowSize:int=8, maxWindowSize:int=50, lower:int=200, upper:int=5000) -> int:
+    """Determine the number of tokens within each chunk
+    
+    Parameters
+    ----------
+    tokenCount : int
+        total number of tokens
+    minWindowSize : int
+        minimum window size 
+    maxWindowSize : int
+        maximum window size 
+    lower : int
+        minimum number of tokens
+    upper : int
+        maximum number of tokens
+
+    Returns
+    -------
+    int
+        number of tokens within each chunk
+
+    """
+
     if tokenCount <= lower:
         return minWindowSize
     elif tokenCount >= upper:
@@ -118,7 +234,21 @@ def ActivationFunction(tokenCount, minWindowSize=8, maxWindowSize=50, lower=200,
     else:
         return minWindowSize + int(((maxWindowSize-minWindowSize) / (upper - lower)) * (tokenCount-lower))
     
-def CreateDSC(htmlContent) -> list:
+def CreateDSC(htmlContent:bytes) -> list[tuple[int,int]]:
+    """Create the dsc graph
+    
+    Parameters
+    ----------
+    htmlContent : bytes
+        byte representation of the html content
+    
+    Returns
+    -------
+    list[tuple[int,int]]
+        dsc graph where the tuple represents each coordinate
+
+    """
+
     soup = BeautifulSoup(htmlContent, 'html.parser')
     
     totalTags, totalTokens = 0, 0 
@@ -127,6 +257,7 @@ def CreateDSC(htmlContent) -> list:
     for tag in soup.find_all():
         if tag.get_text().strip():
             tokens = tokenize(tag.get_text().strip().lower())
+            # tokens = tag.get_text().strip().lower().split()
             totalTags += 1
             for token in tokens:
                 totalTokens += 1
@@ -134,33 +265,46 @@ def CreateDSC(htmlContent) -> list:
     
     return result
 
-def GetContentIntervals(htmlContent, intervals):
-    visitedIndex = set()
+def GetContentOfIntervals(htmlContent:bytes, intervals:list[tuple[int,int]]) -> list[str]:
+    """Get the text within the specified intervals
+    
+    Parameters
+    ----------
+    htmlContent : bytes
+        byte representation of the html content
+    intervals : list[tuple[int,int]]
+        intervals for important text
+    
+    Returns
+    -------
+        list[str]
+            list of important text
+    """
+
     soup = BeautifulSoup(htmlContent, 'html.parser')
     
     result = []
     count = 0
     pointer = 0
-    with open("temp.txt", "w+") as f:
-        for tag in soup.find_all():
-            if tag.get_text().strip():
-                tokens = tokenize(tag.get_text().strip().lower())
-                for token in tokens:
-                    if pointer < len(intervals):
-                        if count > intervals[pointer][1]:
-                            pointer += 1
-                        elif count < intervals[pointer][0]:
-                            pass
-                        else:
-                            f.write(token + " ")
-                            result.append(token)
-                    count += 1
+
+    for tag in soup.find_all():
+        if tag.get_text().strip():
+            tokens = tokenize(tag.get_text().strip().lower())
+            # tokens = tag.get_text().strip().lower().split()
+            for token in tokens:
+                if pointer >= len(intervals):
+                    continue
+                if count > intervals[pointer][1]:
+                    pointer += 1
+                if pointer < len(intervals) and count >= intervals[pointer][0]:
+                    result.append(token)
+                count += 1
                 
     return result
 
 if __name__ == "__main__":
-    resp = requests.get("https://www.webmd.com/cancer/cancer-hiring-caregiver")
-    intervals = FindContentBlock(resp.content)
-    print(GetContentIntervals(resp.content, intervals))
-    print(intervals)
+    # resp = requests.get("https://en.wikipedia.org/wiki/Natural_language_processing")
+    resp = requests.get("https://www.mayoclinic.org/diseases-conditions/abdominal-aortic-aneurysm/symptoms-causes/syc-20350688")
+
+    tokens = FindContentBlock(resp.content)
     
